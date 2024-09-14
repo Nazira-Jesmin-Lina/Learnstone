6f1172a0326433ba3bc439fb86c4a1823eec8009
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2024-09-15 02:11:09 +0600

    modified:   components/Hero.vue

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="noise-bg rounded-b-[2rem] justify-center items-center relative w-full pl-[3.5rem]">
+    <div class="noise-bg rounded-b-[2rem] py-28 justify-center items-center relative w-full pl-[3.5rem]">
         <img src="/Decorative Dots.png" class="absolute bottom-2  w-full lg:w-[1000px] z-0 right-0" />
-        <div class="flex justify-center items-center w-full relative z-20">
+        <div class="flex justify-center  items-center w-full relative z-20">
             <div class="w-full [text-#00140F] ml-20 py-20 justify-center items-center">
                 <h1 class="text-5xl max-w-[671px] font-montserrat font-semibold leading-[56px] pr-16">
                     Empowering Students and Young Professionals to Find the Right Stepping Stone
