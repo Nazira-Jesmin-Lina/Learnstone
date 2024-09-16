b6adc357332bfaeaf623c01fcac017db1fdbb849
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2024-09-16 15:44:00 +0600

    updated version

--- components/OurWork.vue
+++ components/OurWork.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="ourwork"
-    class="px-4 lg:px-0 lg:pl-[3.5rem] 3xl:pl-[4rem] 4xl:pl-[0] bg-[#21463C] lg:flex lg:gap-16 gap:14 relative justify-center items-center pb-0lg:pb-10  z-0 flex flex-col 3xl:w-full">
+    class="px-4 lg:px-0 lg:pl-[3.5rem] 3xl:pl-[4rem] 4xl:pl-[0] bg-[#21463C] lg:flex lg:gap-16 3xl:gap-12 gap:14 relative justify-center items-center pb-0lg:pb-10  z-0 flex flex-col 3xl:w-full">
     <div class=" items-center 3xl:w-2/3 4xl:w-3/5 lg:pt-20 pt-8">
       <div class="flex-col items-center ">
         <h6 class="text-lg font-semibold font-montserrat text-[#FF5F45] text-start w-full items-start">Our Work</h6>
@@ -8,19 +8,21 @@
           Disrupting Current Solutions</h3>
         <div class="flex flex-col lg:flex-row-reverse items-center justify-center 3xl:pl-0 gap-4 3xl:gap-8 ">
           <div class="lg:pr-12 4xl:pr-0 mt-10 relative ">
-            <img src="/our_work_1.png" class="w-[343px] lg:w-[572px] object-contain relative z-20" />
+            <img src="/our_work_1.png" class="w-full lg:w-[572px] object-contain relative z-20" />
             <img src="/Frame 204.png"
               class="absolute -top-12 lg:-top-6 -right-[10px] lg:right-[-4rem] 4xl:right-[-7rem] overflow-hidden lg:transform lg:-translate-y-1/3 z-10 w-[176px] lg:w-[293px] 3xl:right-[-55px]" />
           </div>
-          <div class="flex flex-col w-[343px] lg:w-[572px] 4xl:w-[670px]">
+          <div class="flex flex-col w-full lg:w-[572px] 4xl:w-[670px]">
             <h4 class="font-montserrat text-[#A2FF48] text-2xl font-semibold leading-[36px] text-left lg:mb-10 mb-5">
               The problem we're addressing isn't new.
             </h4>
-            <p class="text-[#ffffff] font-hind text-base font-light leading-[32px] text-left">
+            <p class="text-[#ffffff] font-hind text-base font-thin
+             leading-[32px] text-left">
               Students and young professionals have always faced overwhelming choices that will multiple over time, and
               a lack of clear guidance when planning their next steps.
             </p>
-            <p class="text-[#ffffff] font-hind text-base mt-4 font-light leading-[32px] text-left">
+            <p class="text-[#ffffff] font-hind text-base mt-4 font-thin
+             leading-[32px] text-left">
               Existing solutions often provide superficial information, falling short of what's needed.
             </p>
           </div>
@@ -43,15 +45,15 @@
 
     </div>
 
-    <div class=" items-center 3xl:w-2/3 4xl:w-3/5 mt-10">
+    <div class=" items-center 3xl:w-2/3 4xl:w-3/5 lg:mt-0 mt-10">
       <div class="flex flex-col lg:flex-row items-center justify-center lg:pl-20 3xl:pl-0  gap-8">
         <div class="flex ">
           <div class="">
-            <img src="/ourwork_1.png" class="w-[343px] lg:w-[700px] 3xl:w-[572px] 4xl:w-[650px] object-contain" />
+            <img src="/ourwork_1.png" class="w-full lg:w-[700px] 3xl:w-[572px] 4xl:w-[650px] object-contain" />
           </div>
         </div>
         <div
-          class="flex flex-col lg:pr-10  lg:mr-20 3xl:mr-0 w-[343px] lg:w-[700px] 3xl:w-[620px] 4xl:w-[800px] 4xl:pr-0 4xl:mr-0 3xl:pr-16">
+          class="flex flex-col lg:pr-10  lg:mr-20 3xl:mr-0 w-full lg:w-[700px] 3xl:w-[620px] 4xl:w-[800px] 4xl:pr-0 4xl:mr-0 3xl:pr-16">
           <h4 class="font-montserrat text-[#A2FF48] text-[24px] font-semibold leading-[36px] text-left lg:mb-10 mb-5">
             With Exhaustive Contextual Data
           </h4>
@@ -60,23 +62,25 @@
             starting point.</p>
           <img src="/Separator.png" class="lg:w-[550px] lg:my-5 my-6 " />
           <p
-            class="text-[#ffffff] text-left font-hind text-[16px] font-bold leading-[24px] tracking-[0.0075em] space-y-5 mb-5">
+            class="text-[#ffffff] text-left font-hind text-base font-bold leading-[24px] space-y-5 mb-5">
             The real value comes from contextualizing this data:
           </p>
           <ul
-            class="text-[#ffffff] list-disc pl-5 lg:pl-8 text-left font-hind text-[16px] font-light leading-[24px] tracking-[0.0075em]">
+            class="text-[#ffffff] list-disc pl-5 lg:pl-8 text-left font-hind text-base font-thin
+             leading-[32px]">
             <li>What are people saying about these programs?</li>
             <li>How do employment trends align with program outcomes?</li>
             <li>What do government statistics reveal about job market demand and typical professional trajectories?</li>
           </ul>
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] mt-5">
+          <p class="font-hind text-base font-thin
+           leading-[32px] text-left text-white mt-5">
             By leveraging diverse data sources beyond basic listings, we deliver deeper, more meaningful insights that
             other platforms overlook.
           </p>
           <div class="flex gap-3 mt-8 w-full">
             <NuxtLink to="#contactus" class="w-full lg:w-fit">
               <button
-                class="btn-primary font-hind text-[16px] font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
+                class="btn-primary font-hind text-base font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
                 Contact Us
                 <img class="pt-1" src="/angle_right.svg" />
               </button>
@@ -91,37 +95,39 @@
         class="flex flex-col lg:flex-row-reverse items-center justify-center lg:px-20 3xl:pl-0 gap-4 3xl:gap-14 4xl:gap-8 3xl:pr-16 4xl:pr-0">
         <div class="flex ">
           <div class="lg:pr-14 3xl:pr-0 ">
-            <img src="/ourwork_2.png" class="w-[343px] lg:w-[609px] 3xl:w-[750px] 4xl:w-[609px] object-contain" />
+            <img src="/ourwork_2.png" class="w-full lg:w-[609px] 3xl:w-[750px] 4xl:w-[609px] object-contain" />
           </div>
         </div>
-        <div class="flex flex-col lg:pr-5  3xl:pr-0 w-[343px] lg:w-[700px] 3xl:w-[646px] 4xl:w-[629px] ">
+        <div class="flex flex-col lg:pr-5  3xl:pr-0 w-full lg:w-[700px] 3xl:w-[646px] 4xl:w-[629px] ">
           <h4 class="font-montserrat text-[#A2FF48] text-[24px] font-semibold leading-[36px] text-left lg:mb-10 mb-5">
             Involving Human Expertise
           </h4>
-          <p class="text-[#ffffff] font-montserrat text-[20px] font-medium leading-[32px] text-left">While AI is
+          <p class="text-[#ffffff] font-montserrat text-[20px] font-medium leading-[32px]">While AI is
             powerful and holds great promise, it's not a complete replacement for human insight.</p>
           <img src="/Separator.png" class="lg:w-[550px] lg:my-5 my-6 " />
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] lg:mt-5 mt-0">
+          <p class="font-hind text-base font-thin
+           leading-[32px] text-left text-white  lg:mt-5 mt-0">
             We leverage cutting-edge technology to provide highly personalized recommendations, but we understand that
             reliable high-quality decisions often require a human touch.
           </p>
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] mt-5">
+          <p class="font-hind text-base font-thin
+           leading-[32px] text-left text-white  mt-5">
             That's why we partner with third-party agencies and independent consultants through our marketplace,
             offering users price-competitive, expert advice for the final, critical, steps.
           </p>
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] mt-5">
+          <p class="font-hind text-base font-thin
+           leading-[32px] text-left text-white  mt-5">
             By combining AI-driven insights with human expertise, we set the grounds for the best possible outcomes for
             each user.
           </p>
           <div class="flex gap-3 mt-8">
             <NuxtLink to="#contactus" class="w-full lg:w-fit">
               <button
-                class="btn-primary font-hind text-[16px] font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
+                class="btn-primary font-hind text-base font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
                 Contact Us
                 <img class="pt-1" src="/angle_right.svg" />
               </button>
             </NuxtLink>
-
           </div>
         </div>
       </div>
@@ -130,11 +136,11 @@
       <div class="flex flex-col lg:flex-row items-center justify-center lg:pl-20 3xl:pl-0 gap-8">
         <div class="flex ">
           <div class="">
-            <img src="/ourwork_3.png" class="w-[343px] lg:w-[700px] 3xl:w-[572px] 4xl:w-[650px] object-contain" />
+            <img src="/ourwork_3.png" class="w-full lg:w-[700px] 3xl:w-[572px] 4xl:w-[650px] object-contain" />
           </div>
         </div>
         <div
-          class="flex flex-col lg:pr-10 3xl:pr-16 lg:mr-20 3xl:mr-0 w-[343px] lg:w-[700px] 3xl:w-[646px] 4xl:w-[800px] 4xl:pr-0">
+          class="flex flex-col lg:pr-10 3xl:pr-16 lg:mr-20 3xl:mr-0 w-full lg:w-[700px] 3xl:w-[646px] 4xl:w-[800px] 4xl:pr-0">
           <h4 class="font-montserrat text-[#A2FF48] text-[24px] font-semibold leading-[36px] text-left lg:mb-10 mb-5">
             Leveraging Machine Learning
           </h4>
@@ -143,18 +149,18 @@
             process and model vast amounts of unstructured data at scale.
           </p>
           <img src="/Separator.png" class="w-[550px] 3xl:w-[580px] my-5 " />
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] lg:mt-5 mt-0">
+          <p class="font-hind text-base font-thin leading-[32px] text-left text-white  lg:mt-5 mt-0">
             This enables us to semantically understand and assess learning opportunities in ways that were previously
             unimaginable.
           </p>
-          <p class="text-[#ffffff] text-left font-hind text-[15px] font-normal leading-[24px] tracking-[0.0075em] mt-5">
+          <p class="font-hind text-base font-thin leading-[32px] text-left text-white mt-5">
             We don't just recommend opportunities - we connect you to the ones that genuinely fit your goals and
             context.
           </p>
           <div class="flex gap-3 mt-8">
             <NuxtLink to="#contactus" class="w-full lg:w-fit">
               <button
-                class="btn-primary font-hind text-[16px] font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
+                class="btn-primary font-hind text-base font-normal flex items-center justify-center gap-1 h-10 w-full lg:w-13">
                 Contact Us
                 <img class="pt-1" src="/angle_right.svg" />
               </button>
