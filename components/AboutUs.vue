6be7d5a5cfde0aaa217de78a647790a9cd74a64c
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2024-09-18 16:54:31 +0600

    Frame Updated

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -1,46 +1,46 @@
 <template>
-<div class="px-4 lg:px-0 items-center justify-center w-full">
-          <div id="aboutus" class="bg-[#21463C] lg:pb-14 pb-0 4xl:pb-16 items-center justify-center flex flex-col w-full">
-            <div class=" text-black lg:px-[8.5rem] 3xl:px-[23.5rem] 4xl:px-[27rem] justify-center items-center w-full  ">
-              <h6 class="text-[20px] font-semibold font-montserrat text-[#FF5F45] text-start lg:mt-[5.5rem] mt-10 pb-2">About Us</h6>
-              <h3 class="text-[40px] font-montserrat lg:mb-10 mb-0 leading-[48px] text-[#ffffff] text-start">Understanding the Team</h3>
-              <div class="flex flex-row gap-12 justify-between w-full lg:mb-[-20px]">
-                <div class="flex flex-col-reverse lg:flex-row w-full text-center items-center justify-center gap-8  mt-[1.25rem]">
-                  <div class="text-[#ffffff] text-left font-hind text-lg w-full 4xl:w-11/12 tracking-wide space-y-8 ">
-                    <h5 class="text-[#A2FF48] font-montserrat text-[24px] font-medium leading-[36px] text-left">Why Now?</h5>
-                    <div class="space-y-4">
-                      <p class="font-extralight text-base font-hind">Recent breakthroughs in Natural Language Processing, Machine Learning, and Large Language Models have made it possible to process vast amounts of unstructured data at scale—something that was previously accessible to only a few.</p>
-                      <p class="font-extralight text-base font-hind">Now, this technology is commercially viable, just as the demand for personalized education and career guidance is rising.</p>
-                      <p class="font-extralight text-base font-hind">This convergence makes it the perfect time for a platform that delivers smarter, more contextual recommendations, moving beyond traditional search methods.</p>
+  <div class="px-4 lg:px-0 items-center justify-center w-full">
+            <div id="aboutus" class="bg-[#21463C] lg:pb-14 pb-0 4xl:pb-16 items-center justify-center flex flex-col w-full">
+              <div class=" text-black lg:px-[8.5rem] 3xl:px-[23.5rem] 4xl:px-[27rem] justify-center items-center w-full  ">
+                <h6 class="text-[20px] font-semibold font-montserrat text-[#FF5F45] text-start lg:mt-[5.5rem] mt-10 pb-2">About Us</h6>
+                <h3 class="text-[40px] font-montserrat lg:mb-10 mb-0 leading-[48px] text-[#ffffff] text-start">Understanding the Team</h3>
+                <div class="flex flex-row gap-12 justify-between w-full lg:mb-[-20px]">
+                  <div class="flex flex-col-reverse lg:flex-row w-full text-center items-center justify-center gap-8  mt-[1.25rem]">
+                    <div class="text-[#ffffff] text-left font-hind text-lg w-full 4xl:w-11/12 tracking-wide space-y-8 ">
+                      <h5 class="text-[#A2FF48] font-montserrat text-[24px] font-medium leading-[36px] text-left">Why Now?</h5>
+                      <div class="space-y-4">
+                        <p class="font-extralight text-base font-hind">Recent breakthroughs in Natural Language Processing, Machine Learning, and Large Language Models have made it possible to process vast amounts of unstructured data at scale—something that was previously accessible to only a few.</p>
+                        <p class="font-extralight text-base font-hind">Now, this technology is commercially viable, just as the demand for personalized education and career guidance is rising.</p>
+                        <p class="font-extralight text-base font-hind">This convergence makes it the perfect time for a platform that delivers smarter, more contextual recommendations, moving beyond traditional search methods.</p>
+                      </div>
                     </div>
-                  </div>
-                  <div class="relative w-full 3xl:w-5/6 4xl:pl-6  items-center justify-center ">
-                    <img src="/aboutUs_1.svg" class="relative w-full lg:w-[650px] object-contain z-10 lg:top-[-56px]" />
-                    <!-- <img src="/public/ellipse3.svg" class="absolute -top-[50px] lg:top-20 -right-[0.5rem] lg:right-[4rem] z-0 w-[176px] lg:w-5/6 " /> -->
+                    <div class="relative w-full lg:w-2/3 3xl:w-[63%]  4xl:pl-6  items-center justify-center ">
+                      <img src="/Frame 1000003452.svg" class="relative w-full lg:w-[650px] 4xl:w-[572px] object-contain z-10 lg:top-[-26px] " />
+                      <!-- <img src="/public/ellipse3.svg" class="absolute -top-[50px] lg:top-20 -right-[0.5rem] lg:right-[4rem] z-0 w-[176px] lg:w-5/6 " /> -->
+                    </div>
                   </div>
                 </div>
               </div>
+              <div class="my-auto text-black pt-10 lg:mt-[0px] pb-8 lg:pb-10 3xl:pb-10 lg:px-[8.5rem] 3xl:px-[22rem] 4xl:px-[26rem] justify-center items-center w-full">
+                  <div class="flex flex-col-reverse lg:flex-row-reverse justify-between items-center text-center gap-8">
+                    <div class="text-[#ffffff] text-left font-hind text-lg w-full 4xl:w-5/6 4xl:pl-8 tracking-wide space-y-8 z-10 relative">
+                      <h5 class="text-[#A2FF48] font-montserrat text-[20px] font-medium leading-[24px] text-left">Why Us?</h5>
+                      <div class="space-y-4">
+                        <p class="font-extralight text-base font-hind">Our team is composed of researchers and engineers with exceptional academic and professional track records.</p>
+                        <p class="font-extralight text-base font-hind">Many of us hold top-tier credentials in Natural Language Processing and related fields. We also bring expertise in data collection, processing, and warehousing.</p>
+                        <p class="font-extralight text-base font-hind">This unique combination of skills positions us strongly to deliver on the challenge.</p>
+                      </div>
+                    </div>
+                    <div class="w-full lg:w-2/3 3xl:w-1/2  3xl:pl-6 4xl:pl-4  relative">
+                      <img src="/aboutUs_new.svg" class="w-full lg:w-[650px] 4xl:w-[572px] object-contain z-10 relative" />
+                      <!-- <img src="/Frame 205 (1).svg" class="absolute -bottom-8 -left-3 lg:-bottom-[3.5rem] lg:-left-[4.5rem] lg:transform lg:translate-x-4 z-0 w-[176px] lg:w-[200px]" /> -->
+                    </div>
+                  </div>
+                </div>
             </div>
-            <div class="my-auto text-black pt-10 lg:mt-[-80px] pb-8 lg:pb-10 3xl:pb-10 lg:px-[8.5rem] 3xl:px-[22rem] 4xl:px-[26rem] justify-center items-center w-full">
-                <div class="flex flex-col-reverse lg:flex-row-reverse justify-between items-center text-center gap-8">
-                  <div class="text-[#ffffff] text-left font-hind text-lg w-full 4xl:w-5/6 4xl:pl-8 tracking-wide space-y-8 z-10 relative">
-                    <h5 class="text-[#A2FF48] font-montserrat text-[20px] font-medium leading-[24px] text-left">Why Us?</h5>
-                    <div class="space-y-4">
-                      <p class="font-extralight text-base font-hind">Our team is composed of researchers and engineers with exceptional academic and professional track records.</p>
-                      <p class="font-extralight text-base font-hind">Many of us hold top-tier credentials in Natural Language Processing and related fields. We also bring expertise in data collection, processing, and warehousing.</p>
-                      <p class="font-extralight text-base font-hind">This unique combination of skills positions us strongly to deliver on the challenge.</p>
-                    </div>
-                  </div>
-                  <div class="w-full 3xl:w-5/6 3xl:pl-6 4xl:pl-4  relative">
-                    <img src="/aboutUs_2.svg" class="w-full lg:w-[650px] 4xl:w-[572px] object-contain z-10 relative" />
-                    <!-- <img src="/Frame 205 (1).svg" class="absolute -bottom-8 -left-3 lg:-bottom-[3.5rem] lg:-left-[4.5rem] lg:transform lg:translate-x-4 z-0 w-[176px] lg:w-[200px]" /> -->
-                  </div>
-                </div>
-              </div>
-          </div>
-         </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
+           </div>
+  </template>
+  
+  <script setup lang="ts"></script>
+  
+  <style scoped></style>
